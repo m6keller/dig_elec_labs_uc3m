bfce0ded7cc3bf8973a9514a704efa42c3bf255a
Leonruan <129298868+Leonruan@users.noreply.github.com>
2023-11-02 14:51:29 +0100

    Update die.vhd

--- die.vhd
+++ die.vhd
@@ -1,31 +1,43 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 entity die is
-	port (
-		Reset: in std_logic;
-		Clk: in std_logic;
-		Enable: in std_logic;
-		sel: in unsigned(1 downto 0);
-		seg: out std_logic_vector(6 downto 0); -- Low level active
-		an: out std_logic_vector(3 downto 0) -- Low level active
-	);
+    port (
+        Reset: in std_logic;
+        Clk: in std_logic;
+        Enable: in std_logic;
+        sel: in unsigned(1 downto 0);
+        seg: out std_logic_vector(6 downto 0); -- Low level active
+        an: out std_logic_vector(3 downto 0) -- Low level active
+    );
 end die;
 
-
 architecture Behavioral of die is
-    signal count_reg : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+    signal CounterEnable : std_logic := '0'; -- Signal to control counter enable
+    signal CounterValue : std_logic_vector(3 downto 0); -- Signal to store counter value
+
+    -- Other signal declarations and components (e.g., timer, decoders)
+
 begin
+    -- Counter instantiation
+    counter_instance: entity work.counter
+        port map (
+            Clk => Clk,                 -- Connect to the same clock signal
+            Enable => CounterEnable,    -- Connect to the counter enable signal
+            Count => CounterValue       -- Connect to the output of the counter
+        );
+
+    -- Timer logic to generate a 1 ms enable signal
     process (Clk)
     begin
-        if clk'EVENT AND clk = '1' then
-            if Enable = '1' then
-                if count_reg = "1001" then
-                    count_reg <= "0000";
-                else
-                    count_reg <= count_reg + 1;
-                end if;
-            end if;
-        end if;
+        -- Implement timer logic to generate a 1 ms enable signal
+        -- The timer output should control the CounterEnable signal
     end process;
 
-    Count <= count_reg;
+    -- Display logic (connecting CounterValue to the 7-segment display)
+
+    -- Other VHDL logic for the die entity
 
 end Behavioral;
