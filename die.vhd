0f3fa4560ebb9b31f3d0e2953da863d48f1bf05c
Leonruan <129298868+Leonruan@users.noreply.github.com>
2023-11-02 19:15:31 +0100

    Update die.vhd

--- die.vhd
+++ die.vhd
@@ -15,29 +15,57 @@ entity die is
 end die;
 
 architecture Behavioral of die is
+    -- Signal declarations
     signal CounterEnable : std_logic := '0'; -- Signal to control counter enable
-    signal CounterValue : std_logic_vector(3 downto 0); -- Signal to store counter value
+    signal CounterValue : std_logic_vector(3 downto 0) := "0000"; -- Signal to store counter value
 
-    -- Other signal declarations and components (e.g., timer, decoders)
+    -- Include any other signal declarations and components here
+
+    -- Instantiate the display logic
+    component display_logic is
+        port (
+            sel: in unsigned(1 downto 0);
+            data: in std_logic_vector(3 downto 0);
+            seg: out std_logic_vector(6 downto 0);
+            an: out std_logic_vector(3 downto 0)
+        );
+    end component;
 
 begin
     -- Counter instantiation
     counter_instance: entity work.counter
         port map (
-            Clk => Clk,                 -- Connect to the same clock signal
-            Enable => CounterEnable,    -- Connect to the counter enable signal
-            Count => CounterValue       -- Connect to the output of the counter
+            Clk => Clk,               -- Connect to the same clock signal
+            Enable => CounterEnable,  -- Connect to the counter enable signal
+            Count => CounterValue     -- Connect to the output of the counter
         );
 
-    -- Timer logic to generate a 1 ms enable signal
-    process (Clk)
+    -- Display logic instantiation
+    display_instance: display_logic
+        port map (
+            sel => sel,
+            data => CounterValue,  -- Connect CounterValue to the display data input
+            seg => seg,
+            an => an
+        );
+
+    -- Add any other component instantiations and connections here
+
+    -- Enable control logic
+    process (Reset, Clk, Enable)
     begin
-        -- Implement timer logic to generate a 1 ms enable signal
-        -- The timer output should control the CounterEnable signal
+        if Reset = '1' then
+            CounterEnable <= '0'; -- Reset the enable signal on reset
+        elsif clk’EVENT AND clk = ‘1’  then
+            -- Control the enable signal based on the central button (BTNC)
+            if Enable = '1' then
+                CounterEnable <= '1'; -- Enable the counter
+            else
+                CounterEnable <= '0'; -- Disable the counter
+            end if;
+        end if;
     end process;
 
-    -- Display logic (connecting CounterValue to the 7-segment display)
-
-    -- Other VHDL logic for the die entity
+    -- Add any other VHDL logic for the die entity
 
 end Behavioral;
